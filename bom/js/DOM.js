9d8bd7f30beb9a1711892a6323c9a4235ff59183
jesussanchez1389 <jesussanchez@byui.edu>
2022-02-04 21:48:37 -0500

    New assigment

---
+++ bom/js/DOM.js
@@ -0,0 +1,23 @@
+const input= document.querySelector('#favchap');
+const button= document.querySelector('button');
+const list= document.querySelector('.list');
+
+button.addEventListener('click', ()=>{
+	let chapter = input.value;
+	input.value ='';
+	let listChapter = document.createElement('li');
+    const chapterName = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+	listChapter.appendChild(chapterName);
+    listChapter.textContent = chapter;
+    listChapter.appendChild(listBtn);
+    listBtn.textContent = '❌';
+   	list.appendChild(listChapter);
+
+    listBtn.onclick = function(e) {
+    	list.removeChild(listChapter);
+        }
+
+        input.focus();
+});
